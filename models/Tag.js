7a9d0e4ade70187e0411ecf6abe609ebda6a3362
Jimmy Zheng <jimmyz0622@gmail.com>
2021-10-30 15:10:06 -0700

    Init tag class

--- models/Tag.js
+++ models/Tag.js
@@ -2,19 +2,27 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Tag extends Model { }
 
 Tag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'tag',
-  }
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        tag_name: {
+            type: DataTypes.STRING,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'tag',
+    }
 );
 
 module.exports = Tag;
