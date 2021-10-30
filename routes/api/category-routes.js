7af0575db97bcdab8d10613994c4e6ae9fa62afb
Jimmy Zheng <jimmyz0622@gmail.com>
2021-10-30 16:00:55 -0700

    Include associated products for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,9 @@ router.get('/', async (req, res) => {
     // find all categories
     // be sure to include its associated Products
     try {
-        const categoryData = await Category.findAll();
+        const categoryData = await Category.findAll({
+            include: [{ model: Product }]
+        });
         res.status(200).json(categoryData);
     } catch (err) {
         res.status(500).json(err);
@@ -18,7 +20,9 @@ router.get('/:id', async (req, res) => {
     // find one category by its `id` value
     // be sure to include its associated Products
     try {
-        const categoryData = await Category.findByPk(req.params.id);
+        const categoryData = await Category.findByPk(req.params.id, {
+            include: [{ model: Product }]
+        });
         if (!categoryData) {
             res.status(404).json({ message: 'No category with this id!' });
             return;
@@ -64,19 +68,19 @@ router.delete('/:id', async (req, res) => {
     // delete a category by its `id` value
     try {
         const categoryData = await Category.destroy({
-          where: {
-            id: req.params.id,
-          },
+            where: {
+                id: req.params.id,
+            },
         });
         if (!categoryData) {
-          res.status(404).json({ message: 'No category with this id!' });
-          return;
+            res.status(404).json({ message: 'No category with this id!' });
+            return;
         }
         res.status(200).json(categoryData);
-      } catch (err) {
+    } catch (err) {
         res.status(500).json(err);
-      }
-    
+    }
+
 });
 
 module.exports = router;
