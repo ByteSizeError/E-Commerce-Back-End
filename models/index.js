df266ea1c61f7faff5da10d72aab13080726cf81
Jimmy Zheng <jimmyz0622@gmail.com>
2021-10-30 15:11:38 -0700

    Associate model relations

--- models/index.js
+++ models/index.js
@@ -5,16 +5,29 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+    foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+    through: ProductTag,
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+    through: ProductTag
+});
 
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+    Product,
+    Category,
+    Tag,
+    ProductTag,
 };
