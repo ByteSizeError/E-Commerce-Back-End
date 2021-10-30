827a95f4d330c7d271b255ff43fd73e7f3926ac2
Jimmy Zheng <jimmyz0622@gmail.com>
2021-10-30 15:20:08 -0700

    Associate class relation with foreignKey

--- models/index.js
+++ models/index.js
@@ -18,11 +18,13 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
     through: ProductTag,
+    foreignKey: 'tag_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-    through: ProductTag
+    through: ProductTag,
+    foreignKey: 'product_id',
 });
 
 module.exports = {
